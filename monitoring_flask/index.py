a47ef6839eeec1631143d376b58aa16db090ea1e
Ledu0032 <dusan.vujovic@doodle.com>
2023-04-10 15:03:29 +0200

    changes

--- monitoring_flask/index.py
+++ monitoring_flask/index.py
@@ -2,14 +2,11 @@ from flask import Flask
 from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
-from flask_prometheus_metrics import register_metrics
 
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-
-register_metrics(app, app_version="v0.1.2", app_config="staging")
 
 # Plug metrics WSGI app to your main app with dispatcher
 dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
